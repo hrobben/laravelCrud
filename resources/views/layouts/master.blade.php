f9e377e29cd47a83a4e29747c3eaefa3a7645739
hrobben <hrobben@robusoft.com>
2018-06-18 11:17:26 +0200

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,54 @@
+<!-- Stored in resources/views/layouts/master.blade.php -->
+<html>
+<head>
+    <title>App Name - @yield('title')</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+</head>
+<body>
+    <!--  main menu created here. -->
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Passport Registration</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="/">Home</a>
+            <a class="p-2 text-dark" href="/passports/create">Create</a>
+            <a class="p-2 text-dark" href="/passports">Index</a>
+            <a class="p-2 text-dark" href="#">Pricing</a>
+        </nav>
+
+        @guest
+                <a class="btn btn-outline-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="btn btn-outline-primary" href="{{ route('register') }}">{{ __('Register') }}</a>
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="btn btn-outline-primary dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+
+    </div>
+
+
+    @yield('content')
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title', 'Laravel 5.6 CRUD Tutorial With Example')
+
+@section('content')
+
+    <h1>Welkom!</h1>
+
+@stop
